679b96f59d554f0551a7d1b3a7ae3cd01f777c29
stefan <stefanhitskov@gmail.com>
2015-03-23 03:19:38 +0200

    Test001

--- src/ua/com/jon/Activator.java
+++ src/ua/com/jon/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.BundleContext;
  * The activator class controls the plug-in life cycle
  */
 public class Activator extends AbstractUIPlugin {
-	//Test commit
+	//Test commit123
 	// The plug-in ID
 	public static final String PLUGIN_ID = "jonEclipsePlugin"; //$NON-NLS-1$
 
